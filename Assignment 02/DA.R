d51f7412deeca24c3226ebdf6228e5663fb0f96d
AkshaY2039 <ced15i031@iiitdm.ac.in>
2018-11-29 21:29:37 +0530

    Assignment 1 & 2 Done, Readme Updated

---
+++ Assignment 02/DA.R
@@ -0,0 +1,58 @@
+library(ggplot2)
+library(plotly)
+Country=c("China","India","Canada","United States","Russia","Germany","Kenya")
+CPSPD<-data.frame(read.csv("consumption-per-smoker-per-day.csv", header = TRUE, sep = ","))
+CPSPD<-edit(CPSPD)
+#for a particular country
+Y<-subset(CPSPD,Entity%in%Country)
+ggplotly(ggplot(Y,aes(x=Year,y=cigarette,fill=factor(Code)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_discrete("Code")+
+  xlab("Year")+ylab("Cigarette")+ggtitle("Bar chart of different countries C/S"))
+ggplotly(ggplot(subset(Y,Entity=="India"),aes(x=Year,y=cigarette,fill=factor(Code)))+
+  geom_bar(stat="identity",position="dodge")+
+  scale_fill_discrete("Code")+
+  xlab("Year")+ylab("Cigarette")+ggtitle("C/S for india from 1980-2012"))
+NDS<-data.frame(read.csv("number-of-total-daily-smokers.csv", header = TRUE,sep = ","))
+NDS<-edit(NDS)
+ND <- subset(NDS,Entity%in%Country,select=1:4)
+ND12 <- subset(ND, Year==2012,select=1:4)
+ND00 <- subset(ND, Year==2000,select=1:4)
+ND80 <- subset(ND, Year==1980 ,select=1:4)
+
+par(mfrow=c(1,3))
+pie(ND80$NDS, labels=ND80$Entity)
+pie(ND00$NDS, labels=ND00$Entity)
+pie(ND12$NDS, labels=ND12$Entity)
+
+p<-ggplotly(ggplot(Y, aes(x=Entity, y=cigarette, fill=Code)) +
+    geom_boxplot(width=.01)+geom_violin()+  xlab("Country")+ylab("Cigarettes per smoker")+
+    ggtitle("Violin plot of no. of cigarettes per smoker"))
+p
+i<-ggplotly(ggplot(ND, aes(x=Entity, y=NDS, fill=Code)) +
+              ggtitle("box plot of no. of smokers")+
+  geom_boxplot(outlier.colour="red", outlier.shape=8,
+               outlier.size=4)+  xlab("Country")+ylab("Number of daily smokers"))
+i
+
+CD<-data.frame(read.csv("smoking-and-secondhand-deaths.csv", header = TRUE, sep = ","))
+CD<-subset(CD,Entity%in%Country)
+CD<-edit(CD)
+CDCHN<-subset(CD, Entity=="China")
+ggplotly(ggplot(CDCHN, aes(x=Year, y=SD)) +
+  geom_point(size=2, shape=18, color='red')+
+  geom_smooth(method=lm, linetype="dashed",
+              color="darkred")+ylab("Smoker death")+ggtitle("Scatter- regression of smoker deaths in china"))
+CDIND<-subset(CD, Entity=="India")
+ggplotly(ggplot(CDIND, aes(x=Year, y=SD)) +
+  geom_point(size=2, shape=18, color='Blue')+
+  geom_smooth(method=lm, linetype="dashed",
+              color="darkred")+ylab("Smoker death")+ggtitle("Scatter- regression of smoker deaths in India"))
+SDbA<-data.frame(read.csv("share-of-cancer-deaths-attributed-to-tobacco.csv", header=TRUE,sep=","))
+SDbA<-edit(SDbA)
+SDbA<-subset(SDbA,Year==2010)
+p=ggplotly(ggplot(SDbA, aes(x=SDbA$AgeStd, fill=Entity)) +
+  geom_histogram()+
+    ggtitle("Histogram of Age standardized death % among all countries")+
+    xlab("Age Std %")+ylab("No,of countries"))
+p
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Data-Analtyics-Assignments
-Group No - 06
+#	Data-Analtyics-Assignments
+	Course Code : MAN406T
+	Course Faculty : Dr. Sivaselvan B
+
+##	Domain
+	Addiction Patterns
+
+##	Team
+	Group No - 05
+		-	Akshay Kumar	(CED15I031)
+		-	Dharmesh Harsha (ESD15I018)
+		-	Vivek Khatua	(MFD15I014)
+
+
+##	Data Set Source
+	[Our World in Data](https://ourworldindata.org/smoking)
+
+##	Assignments
+1.	Abstract of Domain & Data Mining Techniques used in the Domain
+2.	Descriptive Statistics in R
+3.	Data Mining Techniques (ARM, Classification & Clustering) using Python
