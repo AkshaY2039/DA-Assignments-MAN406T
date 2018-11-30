15b9e67691c225444c0bee720921efa5873df977
AkshaY2039 <ced15i031@iiitdm.ac.in>
2018-11-30 15:50:48 +0530

    Agglomerative Clustering Done

--- Assignment 03/Clustering/Agglomerative.py
+++ Assignment 03/Clustering/Agglomerative.py
@@ -13,21 +13,15 @@ dataAttributes = []
 Attributes = {
 	"0": {},	# Gender
 	"1": {},	# age
-	"2": {},	# Time Slot 1
-	"3": {},	# Time Slot 2
-	"4": {},	# Time Slot 3
-	"5": {},	# Time Slot 4
-	"6": {},	# Time Slot 5
-	"7": {},	# Time Slot 6
-	"8": {}		# Time Slot 7
+	"2": {},	# Number of cigarettes per day
 }
 
 # age and smoking hour in night
-entityIndex = [1,8]
+entityIndex = [1,2]
 
 # Loading Data
 print ("Data Loading Started"), 
-with open ('../../Dataset/Generated Data Set/daily smoking times.csv') as csvdata: 
+with open ('../../Dataset/Generated Data Set/number of cigarettes per day.csv') as csvdata: 
 	readCSV = csv.reader (csvdata, delimiter=',')
 	next (readCSV)	# Skipping Header content
 	for row in readCSV: 
@@ -41,18 +35,18 @@ with open ('../../Dataset/Generated Data Set/daily smoking times.csv') as csvdat
 			dataAttributes.append (numpy.asarray (datarow))
 		except ValueError: 
 			pass
-feats = ["Age","TimeSlot(0-3)"]
+feats = ["Age","Number of Cigarettes per Day"]
 
 print ("Features: ", feats)
 
 print ("Done Loading Data\n")
 
 # seperating test and training data
-dataAttributes = numpy.asarray (dataAttributes[1000:1050])
+dataAttributes = numpy.asarray (dataAttributes)
 
 print ("Agglomerative Clustering Started")
 linkage = "ward" # ward minimizes the variance of the clusters being merged
-AggCluster = AgglomerativeClustering (linkage = linkage, n_clusters = 2).fit (dataAttributes)
+AggCluster = AgglomerativeClustering (linkage = linkage, n_clusters = 5).fit (dataAttributes)
 print ("Agglomerative Clustering Finished\n")
 
 pyplot.scatter (dataAttributes.T[0], dataAttributes.T[1], c = AggCluster.labels_)
