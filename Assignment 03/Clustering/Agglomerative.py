8af9365acfa66ea6ede3ecbab3bb80294d7ce51d
AkshaY2039 <ced15i031@iiitdm.ac.in>
2018-11-30 15:35:17 +0530

    Agglomerative Clustering Done: Left to test on different dataset

--- Assignment 03/Clustering/Agglomerative.py
+++ Assignment 03/Clustering/Agglomerative.py
@@ -11,20 +11,23 @@ from sklearn.cluster import AgglomerativeClustering
 dataAttributes = []
 
 Attributes = {
-	"2": {},	# Year
-	"3": {},	# 70+ years old (deaths)
-	"4": {},	# 5-14 years old (deaths)
-	"5": {},	# 15-49 years old (deaths)
-	"6": {},	# 50-69 years old (deaths)
-	"7": {}		# Under-5s (deaths)
+	"0": {},	# Gender
+	"1": {},	# age
+	"2": {},	# Time Slot 1
+	"3": {},	# Time Slot 2
+	"4": {},	# Time Slot 3
+	"5": {},	# Time Slot 4
+	"6": {},	# Time Slot 5
+	"7": {},	# Time Slot 6
+	"8": {}		# Time Slot 7
 }
 
-# Year vs 70+ years old death
-entityIndex = [2,3]
+# age and smoking hour in night
+entityIndex = [1,8]
 
 # Loading Data
 print ("Data Loading Started"), 
-with open ('../../Dataset/secondhand-smoke-deaths-by-age.csv') as csvdata: 
+with open ('../../Dataset/Generated Data Set/daily smoking times.csv') as csvdata: 
 	readCSV = csv.reader (csvdata, delimiter=',')
 	next (readCSV)	# Skipping Header content
 	for row in readCSV: 
@@ -33,29 +36,24 @@ with open ('../../Dataset/secondhand-smoke-deaths-by-age.csv') as csvdata:
 				Attributes[str (i)][row[i]] = len (Attributes[str (i)].keys())
 		datarow = []
 		try: 
-			values = row[3]
-			# pValues = []
-			# for value in values: 
-			# 	pValues.append (Attributes[str (3)][value])
-			# p = numpy.sum (pValues) / len (values)
-			# datarow.append (p)
-			datarow.append (values)
+			for i in entityIndex: 
+				datarow.append (Attributes[str (i)][row[i]])
 			dataAttributes.append (numpy.asarray (datarow))
 		except ValueError: 
 			pass
-feats = ["Year","70+ years old (deaths)"]
+feats = ["Age","TimeSlot(0-3)"]
 
 print ("Features: ", feats)
 
 print ("Done Loading Data\n")
 
 # seperating test and training data
-dataAttributes = numpy.asarray (dataAttributes)
+dataAttributes = numpy.asarray (dataAttributes[1000:1050])
 
 print ("Agglomerative Clustering Started")
 linkage = "ward" # ward minimizes the variance of the clusters being merged
-AggCluster = AgglomerativeClustering (linkage = linkage, n_clusters = 5).fit (dataAttributes)
+AggCluster = AgglomerativeClustering (linkage = linkage, n_clusters = 2).fit (dataAttributes)
 print ("Agglomerative Clustering Finished\n")
 
-pyplot.scatter (dataAttributes.T[0], dataAttributes.T[1], c = clustering.labels_)
+pyplot.scatter (dataAttributes.T[0], dataAttributes.T[1], c = AggCluster.labels_)
 pyplot.show ()
